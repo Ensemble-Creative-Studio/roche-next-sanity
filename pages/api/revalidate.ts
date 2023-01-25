8818db9e7c0a84d96e937de864870f8eb4faf4d1
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-01-25 15:56:39 +0100

    slug

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,10 +1,11 @@
 import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook';
 
-const handler = async (req: { headers: { [x: string]: { toString: () => any; }; }; body: { id: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { msg?: string; err?: string; }): void; new(): any; }; }; revalidate: (arg0: string) => any; }) => {
+const handler = async (req: { headers: { [x: string]: { toString: () => any; }; }; body: { id: any; slug: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { msg?: string; err?: string; }): void; new(): any; }; }; revalidate: (arg0: string) => any; }) => {
     const secret = process.env.SANITY_WEBHOOK_SECRET
 //authenticating the webhook
   try {
     const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
+
     if (
       !isValidSignature(
         JSON.stringify(req.body),
@@ -15,13 +16,13 @@ const handler = async (req: { headers: { [x: string]: { toString: () => any; }; 
       return res.status(401).json({ msg: 'Invalid request!' });
 
     //getting payload
-    const { id } = req.body;
+    const { id, slug } = req.body;
     await res.revalidate(`/agency`);
     await res.revalidate(`/brands`);
     await res.revalidate(`/`);
     await res.revalidate(`/contact`);
     await res.revalidate(`/legal`);
-    await res.revalidate(`/agency/[slug]`);
+    await res.revalidate(`/agency/${slug}`);
 
     
     res.status(200).json({ msg: 'Product pages revalidated.' });
