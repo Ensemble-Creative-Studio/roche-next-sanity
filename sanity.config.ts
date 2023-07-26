ca5395a71b6f50200b323f1085c410bf9634a5fc
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-07-26 12:09:50 +0200

    added dashboard netlify

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,9 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
+import { dashboardTool } from "@sanity/dashboard";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
+
 import {schemaTypes} from './schemas'
 import StudioNavbar from './app/(user)/components/StudioNavbar'
 const projectId = 'reejd2e7';
@@ -14,7 +17,25 @@ export default defineConfig({
   projectId,
   dataset,
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(),
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+            title: 'My Netlify deploys',
+            sites: [
+           
+              {
+                title: 'Roche',
+                apiId: '8ae1d4e4-c135-47d6-938d-513e0227fe7d',
+                buildHookId: '64c0c8eb6194b335a98edecf',
+                name: 'imaginative-cupcake-ba7eaa',
+                url:'https://imaginative-cupcake-ba7eaa.netlify.app/'
+              }
+            ]
+        })
+      ]
+    })],
+  
   studio:{
 components: {
   navbar: StudioNavbar,
