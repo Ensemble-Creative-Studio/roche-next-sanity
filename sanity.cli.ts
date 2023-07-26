06aedbae0b2dbec73378dad280079955b795c238
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2023-07-26 09:05:40 +0200

    local variable added

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import {defineCliConfig} from 'sanity/cli'
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const projectId = 'reejd2e7';
+const dataset = "production";
 export default defineCliConfig({
   api: {
     projectId,
--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import StudioNavbar from './app/(user)/components/StudioNavbar'
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const projectId = 'reejd2e7';
+const dataset = "production";
 
 export default defineConfig({
   basePath: "/studio",
